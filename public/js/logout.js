e53e0ffbb21fcfed35dc1bb1d07a20a0f088404f
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-31 15:21:25 -0400

    connect to jawsdb

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,5 @@
 // taken from UPENN-VIRT-FSF-FT-07-2022-U-LOLC\14-MVC\01-Activities\28-Stu_Mini-Project\Main\public\js\logout.js
+//added  replace with /login
 const logout = async () => {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
