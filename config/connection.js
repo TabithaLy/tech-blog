664a3d316be4a33a2cd8bb4a1feb0231be65d112
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-30 23:01:18 -0400

    more boiler-plate stuff from past activities added

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// taken from UPENN-VIRT-FSF-FT-07-2022-U-LOLC/14-MVC/01-Activities/28-Stu_Mini_Project/Main/config/connection.js
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
